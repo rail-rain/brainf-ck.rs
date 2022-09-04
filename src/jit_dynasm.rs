6771d4436cb950b0bda5a4eb1ca14fd14165222b
rail-rain <12975677+rail-rain@users.noreply.github.com>
2022-09-05 10:50:25 +1200

    Implemented two types of JIT compilers

---
+++ src/jit_dynasm.rs
@@ -0,0 +1,78 @@
+use crate::{getchar, putchar, Consumer as _};
+use dynasm::dynasm;
+use dynasmrt::{DynasmApi, DynasmLabelApi, ExecutableBuffer};
+
+#[cfg(target_arch = "x86_64")]
+macro_rules! my_dynasm {
+    ($ops:ident $($t:tt)*) => {
+        dynasm!($ops
+            ; .arch x64
+            ; .alias pointer, rbx
+            $($t)*
+        )
+    }
+}
+
+pub fn compile(program: &[u8]) -> ExecutableBuffer {
+    let mut ops = dynasmrt::x64::Assembler::new().unwrap();
+
+    my_dynasm!(ops
+        ; push rbp
+        ; mov rbp, rsp
+
+        ; push pointer
+        ; mov pointer, rdi
+    );
+
+    let mut loops = Vec::new();
+
+    let mut iter = program.iter();
+    while let Some(&c) = iter.next() {
+        match c {
+            b'>' => my_dynasm!(ops; add pointer, (iter.consume_while(b'>') + 1) as _),
+            b'<' => my_dynasm!(ops; sub pointer, (iter.consume_while(b'<') + 1) as _),
+            b'+' => my_dynasm!(ops; add BYTE [pointer], (iter.consume_while(b'+') + 1) as _),
+            b'-' => my_dynasm!(ops; sub BYTE [pointer], (iter.consume_while(b'-') + 1) as _),
+            b'.' => my_dynasm!(ops
+                ; mov rdi, [pointer]
+                ; mov rax, QWORD putchar as _
+                ; call rax
+            ),
+            b',' => my_dynasm!(ops
+                ; mov rax, QWORD getchar as _
+                ; call rax
+                ; mov [pointer], rax
+            ),
+            b'[' => {
+                let bwd_label = ops.new_dynamic_label();
+                let fwd_label = ops.new_dynamic_label();
+                loops.push((bwd_label, fwd_label));
+                my_dynasm!(ops
+                    ; cmp BYTE [pointer], 0
+                    ; je =>fwd_label
+                    ;=>bwd_label
+                )
+            }
+            b']' => {
+                if let Some((bwd_label, fwd_label)) = loops.pop() {
+                    my_dynasm!(ops
+                        ; cmp BYTE [pointer], 0
+                        ; jne =>bwd_label
+                        ;=>fwd_label
+                    )
+                }
+            }
+            _ => {}
+        }
+    }
+
+    my_dynasm!(ops
+        ; pop pointer
+
+        ; mov rsp, rbp
+        ; pop rbp
+        ; ret
+    );
+
+    ops.finalize().unwrap()
+}
--- src/main.rs
+++ src/main.rs
@@ -1,46 +1,65 @@
-use std::io::{self, Write, Read};
+// TODO: do testing with http://www.brainfuck.org/tests.b.
+// unit test or integration test?
 
-enum Instruction {
-    IncrementPointer,
-    DecrementPointer,
-    IncrementData,
-    DecrementData,
-    Output,
-    Input,
-    LoopStart,
-    LoopEnd(usize),
+mod jit_dynasm;
+mod jit_plain;
+
+use std::{io::{self, Read, Write}, mem};
+
+trait Consumer {
+    fn consume_while(&mut self, target: u8) -> usize;
 }
 
-fn run(instructions: &[u8]) {
-    let mut instruction_pointer = 0;
-    let mut data = [0u8; 30_000];
-    let mut data_pointer = 0;
-    let mut stamp = Vec::new();
+impl Consumer for std::slice::Iter<'_, u8> {
+    #[inline(always)]
+    fn consume_while(&mut self, target: u8) -> usize {
+        // Calculate the span of continuous copies of the target.
+        let mut clone = self.clone();
+        while clone.next().copied() == Some(target) {}
+        let span = (self.len() - clone.len()).saturating_sub(1);
 
-    while instruction_pointer == instructions.len() {
-        match instructions[instruction_pointer] {
-            b'>' => data_pointer += 1,
-            b'<' => data_pointer -= 1,
-            b'+' => data[data_pointer] += 1,
-            b'-' => data[data_pointer] -= 1,
-            b'.' => io::stdout().write_all(&data[data_pointer..data_pointer+1]).unwrap(),
-            b',' => io::stdin().read_exact(&mut data[data_pointer..data_pointer+1]).unwrap(),
-            b'[' => if data[data_pointer] == 0 {
-                unimplemented!()
-            } else {
-                stamp.push(instruction_pointer)
-            },
-            b']' => if data[data_pointer] != 0 {
-                instruction_pointer = *stamp.last().unwrap()
-            } else {
-                stamp.pop();
-            },
-            _ => ()
+        // TODO: unstable `Iterator::advance_by` is better.
+        // Need https://github.com/rust-lang/rust/issues/77404
+        if span != 0 {
+            self.nth(span - 1);
         }
-        instruction_pointer += 1;
+        // self.advance_by(span);
+        return span;
+    }
+}
+
+pub extern "sysv64" fn putchar(char: u8) {
+    io::stdout().write_all(&[char]).unwrap()
+}
+
+pub extern "sysv64" fn getchar() -> u8 {
+    let mut buf = [0; 1];
+    io::stdin().read_exact(&mut buf).unwrap();
+    buf[0]
+}
+
+pub fn run(opcode: impl AsRef<[u8]>) {
+    inner(opcode.as_ref());
+    fn inner(opcode: &[u8]) {
+        // The safety depends on the correctness of the compilers. How dangerous.
+        let execute: extern "sysv64" fn(*mut u8) = unsafe { mem::transmute(opcode.as_ptr()) };
+        
+        let mut array = [0; 30_000];
+        execute(array.as_mut_ptr());
     }
 }
 
 fn main() {
-    println!("Hello, world!");
+    // use std::io;
+
+    // let mut buf = String::new();
+    // io::stdin().read_line(&mut buf).unwrap();
+    // interpreter::run(buf.as_bytes(), io::stdout(), io::stdin()).unwrap();
+
+    let program = b"++++++++[>++++[>++>+++>+++>+<<<<-]>+>+>->>+[<]<-]>>.>---.+++++++..+++.>>.<-.<.+++.------.--------.>>+.>++.";
+
+    // pretty_assertions::assert_eq!(&*jit_dynasm::compile(parse(program)), &*jit_plain::compile(parse(program)));
+    run(&*jit_dynasm::compile(program));
+    run(jit_plain::compile(program));
+
 }
