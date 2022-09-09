45902b009a818fee938acaafd23b52af22302ac0
rail-rain <12975677+rail-rain@users.noreply.github.com>
2022-09-10 11:23:02 +1200

    Increase the privacy of jit impls

--- src/jit/mod.rs
+++ src/jit/mod.rs
@@ -1,5 +1,5 @@
-pub mod dynasm;
-pub mod plain;
+mod dynasm;
+mod plain;
 
 use std::mem;
 
@@ -11,13 +11,18 @@ pub extern "sysv64" fn getchar() -> u8 {
     crate::getchar()
 }
 
-pub fn run(opcode: impl AsRef<[u8]>) {
-    inner(opcode.as_ref());
-    fn inner(opcode: &[u8]) {
-        // The safety depends on the correctness of the compilers. How dangerous.
-        let execute: extern "sysv64" fn(*mut u8) = unsafe { mem::transmute(opcode.as_ptr()) };
+fn run_inner(opcode: &[u8]) {
+    // The safety depends on the correctness of the compilers. How dangerous.
+    let execute: extern "sysv64" fn(*mut u8) = unsafe { mem::transmute(opcode.as_ptr()) };
 
-        let mut array = [0; 30_000];
-        execute(array.as_mut_ptr());
-    }
+    let mut array = [0; 30_000];
+    execute(array.as_mut_ptr());
+}
+
+pub fn run_dynasm(program: &[u8]) {
+    run_inner(&dynasm::compile(program))
+}
+
+pub fn run_plain(program: &[u8]) {
+    run_inner(&plain::compile(program))
 }
