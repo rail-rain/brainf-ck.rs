33b6eda6a038c423455a16fe651ba3f72875d568
rail-rain <12975677+rail-rain@users.noreply.github.com>
2023-10-18 14:53:42 +1300

    Setup modules to support mutiple architectures
    This also makes the code compile on aarch64 machines

---
+++ src/jit/aarch64/mod.rs
@@ -0,0 +1,14 @@
+
+pub mod machine {
+    use crate::Error;
+    pub fn run(opcode: &[u8]) -> Result<(), Error> {
+        todo!("The aarch64 backend is not yet implemented");
+    }
+}
+
+pub mod asm {
+    use crate::Error;
+    pub fn run(opcode: &[u8]) -> Result<(), Error> {
+        todo!("The aarch64 backend is not yet implemented");
+    }
+}
--- src/jit/x86_64/asm.rs
+++ src/jit/x86_64/asm.rs
@@ -1,5 +1,3 @@
-#![cfg(target_arch = "x86_64")]
-
 use crate::{
     jit::{getchar, putchar},
     Consumer as _, Error,
